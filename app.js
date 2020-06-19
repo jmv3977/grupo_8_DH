403720de702de058644df8dadbe227f36e42046d
gonzasabe1 <67107012+gonzasabe1@users.noreply.github.com>
2020-06-18 23:35:20 -0300

    cambios
    sumo cambios

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const methodOverride =  require('method-override'); // Pasar poder usar los mét
 const app = express();
 
 // ************ Middlewares - (don't touch) ************
-app.use(express.static(path.join(__dirname, '../public')));  // Necesario para los archivos estáticos en el folder /public
+app.use(express.static(path.join(__dirname, '/public')));  // Necesario para los archivos estáticos en el folder /public
 app.use(express.urlencoded({ extended: false }));
 app.use(logger('dev'));
 app.use(express.json());
