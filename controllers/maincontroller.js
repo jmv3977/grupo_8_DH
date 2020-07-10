236fbb23174a884178e6c268668c96d0f8626fce
alejandrodh <alejandro@digitalhouse.com>
2020-07-10 18:30:57 -0300

    fix de productos y categorias para la home. Carpeta '/product' para las imgagenes de producto.

--- controllers/maincontroller.js
+++ controllers/maincontroller.js
@@ -1,15 +1,19 @@
-const { Product } = require('../database/models');
+const { Product, Category } = require('../database/models');
 
 const controller = {
 	root: (req, res) => {
-		
-		Product.findAll({
+		let products = Product.findAll({
 			limit: 3
-		}) 
-		     .then(productos => {
-				 return res.render('main', {productos})
-			 })
-	},    
+		});
+		let categories = Category.findAll();
+
+		Promise.all([products, categories])
+		.then(data => {
+			res.locals.categories = data[1];
+			// return res.send(data[0]);
+			return res.render('main', { products:data[0] })
+		})
+},    
 	search: (req, res) => {     
 		///////falta el armar el codigo del search!!               
 		return res.render('results', {productos})
