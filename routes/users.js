f2bf6533d2332037140053903e603a2006b9dbcf
AgustinCasas <casas.agustin99@gmail.com>
2020-07-21 09:02:53 -0300

    session

--- routes/users.js
+++ routes/users.js
@@ -9,6 +9,9 @@ const usersController = require('../controllers/usersController');
 router.get('/register', usersController.register); /* GET - home page */
 router.post('/register', usersController.processRegister); /* GET - search results */
 router.get('/login', usersController.login); /* GET - search results */
-router.post('/login', usersController.processLogin); /* GET - search results */
+//router.post('/login', [
+    //check('email').isEmail().withMessage('Email invalido'),
+    //check('password').isLength({min: 8}).withMessage('La contraseña debe tener al menos 8 caracteres')
+//], usersController.processLogin); /* GET - search results */
 
 module.exports = router;
